52c955de2ab46c6ee44b4b4ff6c51afe8fddc181
YoukaiDragon <tyson.foster@hotmail.com>
2024-07-20 12:31:41 -0700

    print word and char data to console

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
 def main():
     book_path = "books/frankenstein.txt"
-    
     text = get_book_text(book_path)
-    print(text)
-    
     word_count = get_word_count(text)
-    print(f"{word_count} words found")
-
     char_counts = get_char_counts(text)
-    print(char_counts)
-
-
+    print_report(book_path, word_count, dict_to_list(char_counts))
 
 def get_book_text(path):
     with open(path) as f:
@@ -30,5 +23,26 @@ def get_char_counts(text):
         else:
             char_counts[c] = 1
     return char_counts
+
+def sort_on(dict):
+    return dict["count"]
+
+def dict_to_list(dict):
+    dict_list = list()
+    for k in dict:
+        dict_list.append({"char": k, "count": dict[k]})
+    return dict_list
+
+def print_report(book, word_count, char_count):
+
+    char_count.sort(reverse=True, key=sort_on)
+
+    print(f"--- Begin report of {book} ---")
+    print(f"{word_count} words found in the document")
+    for c in char_count:
+        if c["char"].isalpha():
+            print(f"The {c['char']} character was found {c['count']} times")
+    print("--- End report ---")
+    return
 
 main()
